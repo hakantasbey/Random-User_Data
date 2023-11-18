ddecdb25ad9417ea1e79b3f5467a869b70ec6f62
hakan <tasbeyhakan@gmail.com>
2023-11-18 17:44:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const nameSpn = document.querySelector(".name")
+const emailSpn = document.querySelector(".email")
+const phoneSpn = document.querySelector(".phone")
+const pictureSrc = document.querySelector(".pictureSrc")
+const API = "https://randomuser.me/api/";
+
